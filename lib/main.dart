4aa523eb4d1b6e5e557995441a7ba77fd3f473b0
Your Name <you@example.com>
2022-07-16 09:23:38 +0530

    ch2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,43 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
-      body: Container(
-        child: Center(child: Text("Hi Flutter")),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Align(
+          alignment: Alignment.center,
+          child: Container(
+            color: Colors.black,
+            width: 200,
+            height: 400,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    color: Colors.green,
+                    width: 100,
+                    height: 100,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    color: Colors.yellow,
+                    width: 100,
+                    height: 100,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    color: Colors.red,
+                    width: 100,
+                    height: 100,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
