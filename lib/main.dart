ff8030d6030da7a42996179b3d98612caeb413e4
Your Name <you@example.com>
2022-07-16 10:05:06 +0530

    ch3 complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "Awesome App",
-    home: HomePage(),
-  ));
+  runApp(
+    MaterialApp(
+      title: "Awesome App",
+      home: HomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+    ),
+  );
 }
 
 class HomePage extends StatelessWidget {
@@ -14,43 +19,37 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Align(
-          alignment: Alignment.center,
-          child: Container(
-            color: Colors.black,
-            width: 200,
-            height: 400,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.green,
-                    width: 100,
-                    height: 100,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.yellow,
-                    width: 100,
-                    height: 100,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.red,
-                    width: 100,
-                    height: 100,
-                  ),
-                ],
+      body: Container(),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text("Dhruv Gupta"),
+              accountEmail: Text("Dhruvguptaalgo@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://images.unsplash.com/photo-1644982647531-daff2c7383f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"),
               ),
             ),
-          ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Account"),
+              subtitle: Text("Personal"),
+              trailing: Icon(Icons.edit),
+            ),
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text("Email"),
+              subtitle: Text("dhruvguptaalgo@gmail.com"),
+              trailing: Icon(Icons.send),
+            ),
+          ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.edit),
       ),
     );
   }
