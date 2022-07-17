3aaaf6dd432c1c2442eb0f1231a70d863c8a63e8
Your Name <you@example.com>
2022-07-17 18:28:56 +0530

    ch5 completed but facing some issue

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:codecharmapp/change-name-card.dart';
+import 'package:http/http.dart' as http;
 import 'package:codecharmapp/drawer.dart';
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   @override
@@ -11,26 +12,47 @@ class _HomePageState extends State<HomePage> {
   TextEditingController _nameController = TextEditingController();
 
   var myText = "Change me";
+  final Uri url = Uri.parse("https://jsonplaceholder.typicode.com/posts");
+  var data;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+    getData();
+  }
+
+  getData() async {
+    var res = await http.get(url);
+    data = jsonDecode(res.body);
+    print(data);
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
+      backgroundColor: Colors.white24,
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child:
-              change_name_card(myText: myText, nameController: _nameController),
-        ),
+        child: data != null
+            ? ListView.builder(
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListTile(
+                      title: Text(data[index]["title"]),
+                      leading: Image.network(data[index]["image"]),
+                    ),
+                  );
+                },
+                itemCount: data.length,
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: MyDrawer(),
       floatingActionButton: FloatingActionButton(
